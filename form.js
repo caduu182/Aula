8d59d645eed86a687bc1f2c13a8ba97d38eb647a
Carlos Eduardo dos Santos Costa <JAC0186065@aluno.firjansenaisesi.com.br>
2025-06-23 21:34:29 -0300

    Meu primeiro formulario

---
+++ form.js
@@ -0,0 +1,7 @@
+btnenviar.addEventListener('click', function(event) {
+    let nome = document.getElementById('nome').value;
+    let idade = document.getElementById('idade').value;
+    
+    console.log(nome);
+    console.log(idade);
+})
